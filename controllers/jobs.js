39cdefee8316909f75db3fa0323be91a7d6d430b
omar42s <128640295+omar42s@users.noreply.github.com>
2023-05-05 02:07:55 +0300

    initial commit

---
+++ controllers/jobs.js
@@ -0,0 +1,77 @@
+const Job= require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError, NotFoundError } = require("../errors");
+const notFound = require('../middleware/not-found');
+
+
+
+const getAllJobs = async (req,res)=>{
+   const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+   res.status(StatusCodes.OK).json({jobs,count: jobs.length})
+}       
+
+const getJob = async (req,res)=>{
+    console.log(req.user )
+
+   const {user:{userId},params:{id: jobId}} = req
+   const job = await Job.findOne({
+    _id:jobId,
+    createdBy:userId
+   })
+   if(!job){
+    throw new NotFoundError(`no job with id ${jobId}`)
+   }
+   res.status(StatusCodes.OK).json({job})
+
+}
+
+const createJob = async (req,res)=>{
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body) 
+    res.status(StatusCodes.CREATED).json(job)
+}
+
+const updateJob = async (req, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  if (company === "" || position === "") {
+    throw new BadRequestError("company or position fields cannot be empty");
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`no job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
+};
+
+const deleteJob = async (req,res)=>{
+    const {
+      user: { userId },
+      params: { id: jobId },
+    } = req;
+    const job = await Job.findByIdAndRemove({
+        _id: jobId,
+        createdBy: userId
+    })
+    if (!job) {
+      throw new NotFoundError(`no job with id ${jobId}`);
+    }
+    res.status(StatusCodes.OK).send()
+}
+
+
+
+module.exports={
+   getAllJobs,
+   getJob,
+   createJob,
+   updateJob,
+   deleteJob
+}
