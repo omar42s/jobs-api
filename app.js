fc41c904d594ebe8858969233d73aaf7ba279c23
omar42s <128640295+omar42s@users.noreply.github.com>
2023-05-05 15:35:52 +0300

    inital commit

--- app.js
+++ app.js
@@ -33,8 +33,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors())
 app.use(xss())
-// extra packages
 
+app.get('/',(req,res)=>{
+  res.send('jobs api trial')
+})
 // routes
 app.use('/api/v1/auth',authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
